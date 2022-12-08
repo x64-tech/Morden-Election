5a265278eb340ce934897bee4bb1e17b5fabddbf
Ankit <code7lancer>
2022-12-08 04:19:13 -0800

    created election details page

---
+++ app/src/main/java/com/x64tech/mordenelection/pages/ElectionDetails.java
@@ -0,0 +1,16 @@
+package com.x64tech.mordenelection.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.x64tech.mordenelection.R;
+
+public class ElectionDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_election_details);
+    }
+}
