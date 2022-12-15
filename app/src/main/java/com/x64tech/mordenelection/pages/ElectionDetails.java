f982dc7f2b306f18c3aaa3002f61e3aa663e8e63
Ankit <code7lancer>
2022-12-15 13:32:29 -0800

    done with candidate listing in election details

--- app/src/main/java/com/x64tech/mordenelection/pages/ElectionDetails.java
+++ app/src/main/java/com/x64tech/mordenelection/pages/ElectionDetails.java
@@ -1,6 +1,9 @@
 package com.x64tech.mordenelection.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +12,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.x64tech.mordenelection.R;
+import com.x64tech.mordenelection.adapters.CandidateAdapter;
 import com.x64tech.mordenelection.extras.SharedPrefHelper;
 import com.x64tech.mordenelection.models.ElectionModel;
 
@@ -16,6 +20,9 @@ public class ElectionDetails extends AppCompatActivity {
 
     TextView detailElectionName, detailElectionDic, detailElectionStart;
     SharedPrefHelper sharedPrefHelper;
+    ElectionModel electionModel;
+    CandidateAdapter candidateAdapter;
+    RecyclerView candidateRecycle;
     Button multiButton;
     String phase;
     Intent intent;
@@ -24,6 +31,7 @@ public class ElectionDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_election_details);
         initVar();
+        setRecycle();
     }
 
     private void initVar() {
@@ -31,8 +39,9 @@ public class ElectionDetails extends AppCompatActivity {
         detailElectionDic = findViewById(R.id.detailDicription);
         detailElectionStart = findViewById(R.id.detailStartDate);
         multiButton = findViewById(R.id.multiButton);
+        candidateRecycle = findViewById(R.id.candidateRecycle);
 
-        ElectionModel electionModel = (ElectionModel) getIntent().getSerializableExtra("election");
+        electionModel = (ElectionModel) getIntent().getSerializableExtra("election");
         phase = getIntent().getStringExtra("phase"); // TODO calculate phase with date in models
         sharedPrefHelper = new SharedPrefHelper(this);
 
@@ -55,5 +64,20 @@ public class ElectionDetails extends AppCompatActivity {
         }else {
             multiButton.setVisibility(View.INVISIBLE);
         }
+    }
+
+    private void setRecycle() {
+        candidateAdapter = new CandidateAdapter(this, electionModel.getCandidates(),
+                sharedPrefHelper);
+        candidateRecycle.setAdapter(candidateAdapter);
+        candidateRecycle.addItemDecoration(
+                new DividerItemDecoration(this,
+                        DividerItemDecoration.HORIZONTAL)
+        );
+        candidateRecycle.setLayoutManager(
+                new LinearLayoutManager(this,
+                        RecyclerView.VERTICAL, false)
+        );
+
     }
 }
